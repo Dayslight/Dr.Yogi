04225976d152c93c43d03bbd7940b4c95044ad9c
bijaybogati <bjdayslight@gmail.com>
2015-12-02 17:39:48 +0545

    inner appointment added

---
+++ app/src/main/java/activity/AppointmentDetailsActivity.java
@@ -0,0 +1,15 @@
+package activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dryogi.R;
+
+public class AppointmentDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_details);
+    }
+}
--- app/src/main/java/fragment/AppointmentFragment.java
+++ app/src/main/java/fragment/AppointmentFragment.java
@@ -1,15 +1,31 @@
 package fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.dryogi.R;
 
+import java.util.ArrayList;
+
+import activity.AppointmentDetailsActivity;
+import adapter.AppointmentListAdapter;
+
 
 public class AppointmentFragment extends Fragment {
+
+
+    ListView appointmentlist;
+
+    ArrayList<String> arrayallappointmentlist = new ArrayList<String>();
+
+    AppointmentListAdapter appointmentListAdapter;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -17,6 +33,31 @@ public class AppointmentFragment extends Fragment {
         // Inflate the layout for this fragment
 
         View vi = inflater.inflate(R.layout.fragment_appointment, container, false);
+
+        appointmentlist = (ListView) vi.findViewById(R.id.appointmentlist);
+
+
+        arrayallappointmentlist.add("1");
+        arrayallappointmentlist.add("2");
+        arrayallappointmentlist.add("3");
+        arrayallappointmentlist.add("4");
+        arrayallappointmentlist.add("5");
+
+
+        appointmentListAdapter = new AppointmentListAdapter(getActivity(), arrayallappointmentlist);
+        appointmentlist.setAdapter(appointmentListAdapter);
+
+
+        appointmentlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = new Intent(getActivity(), AppointmentDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         return vi;
     }
 
